dc42d1d0f75e43f39cdec492833f94742c40c6e4
Mealedey In <inmeallie@gmail.com>
2020-05-22 16:36:12 +0200

    Mister Cocktail -Day2

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,26 +1,34 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:new, :create]
   def new
-    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @dose
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to new_cocktail_dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktails_path, notice: 'Dose was successfully destroyed.'
   end
 
   private
 
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
   def dose_params
-    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,5 @@ class Ingredient < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, presence: true
   has_many :doses
+  has_many :cocktails, through: :doses
 end
